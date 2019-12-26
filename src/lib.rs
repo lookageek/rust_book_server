73bbdd4dc5a4e7af6988e09c48a365f63f11dae1
Jayanth Manklu <jayanthms7@gmail.com>
2019-12-26 23:08:09 +0530

    Get threadpool to working state
    - to move closure out of Box, need to implement a new Trait, FnBox which has method call_box
    - Implement call_box of FnBox on FnOnce, inside call_box move the closure FnOnce out of box and call it by taking ownership

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::thread;
+use std::sync::{mpsc, Arc, Mutex};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 pub struct Worker {
@@ -9,11 +11,33 @@ pub struct Worker {
     thread: thread::JoinHandle<()>,
 }
 
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {} got a job, executing.", id);
+
+                job.call_box();
+            }
+        });
+
         Worker {
             id,
-            thread: thread::spawn(|| {}),
+            thread,
         }
     }
 }
@@ -29,16 +53,20 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for i in 0..size {
-            let worker = Worker::new(i);
+            let worker = Worker::new(i, Arc::clone(&receiver));
             workers.push(worker);
 
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender,
         }
     }
 
@@ -46,6 +74,8 @@ impl ThreadPool {
         where
             F: FnOnce() + Send + 'static
     {
+        let job = Box::new(f);
 
+        self.sender.send(job).unwrap();
     }
 }
