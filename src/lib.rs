f2a737071f4e4eab3f1d9552d6bd679bb192e2bb
Jayanth Manklu <jayanthms7@gmail.com>
2019-12-26 21:24:28 +0530

    Create a thread pool (which currently does nothing)
    - Create a library crate to hold the thread pool implementation
    - Move main.rs to a binary crate under src/bin
    - Thread pool implementation currently creates a vector of Workers at startup and holds in struct ThreadPool
    - A Worker struct holds on to a thread with empty closure

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
+
+pub struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        Worker {
+            id,
+            thread: thread::spawn(|| {}),
+        }
+    }
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut workers = Vec::with_capacity(size);
+
+        for i in 0..size {
+            let worker = Worker::new(i);
+            workers.push(worker);
+
+        }
+
+        ThreadPool {
+            workers
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static
+    {
+
+    }
+}
